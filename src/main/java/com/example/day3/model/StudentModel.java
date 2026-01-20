ee7c62f90224075d110accc9f4c0881f84b29ab6
Akshika Gupta <akshika27ak@gmail.com>
2026-01-20 12:49:01 +0530

    Student Controller

---
+++ src/main/java/com/example/day3/model/StudentModel.java
@@ -0,0 +1,22 @@
+package com.example.day3.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Collection;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor //optional
+@Document(collection = "students")
+public class StudentModel {
+    @Id
+    private String id;
+    private String name;
+    private int age;
+    private String email;
+
+}
